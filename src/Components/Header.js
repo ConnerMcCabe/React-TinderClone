b721cb4ac95fdb3f158f65b733b9575d0a1d01b5
Conner McCabe <52429712+ConnerMcCabe@users.noreply.github.com>
2020-08-17 14:22:17 -0500

    adding iconButton to header for click

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,15 +1,22 @@
 import React from "react";
 import "./Header.css";
 import PersonIcon from "@material-ui/icons/Person";
-import BathtubIcon from "@material-ui/icons/Bathtub";
+import FastfoodIcon from "@material-ui/icons/FastfoodTwoTone";
 import FourumIcon from "@material-ui/icons/Forum";
+import IconButton from "@material-ui/core/IconButton";
 
 function Header() {
   return (
     <div className="header">
-      <PersonIcon />
-      <BathtubIcon />
-      <FourumIcon />
+      <IconButton>
+        <PersonIcon className="headerIcon" color="primary" fontSize="large" />
+      </IconButton>
+
+      <FastfoodIcon className="headerIcon" color="secondary" fontSize="large" />
+
+      <IconButton>
+        <FourumIcon className="headerIcon" color="primary" fontSize="large" />
+      </IconButton>
     </div>
   );
 }
