046cc3ca1c50f770c8ebe28e4c3c28e7b67d4d73
Conner McCabe <52429712+ConnerMcCabe@users.noreply.github.com>
2020-08-17 15:55:47 -0500

    adding dummy info to display card

--- src/App.js
+++ src/App.js
@@ -2,20 +2,21 @@ import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./styles.css";
 import Header from "./Components/Header";
+import TinderCards from "./Components/TinderCards/TinderCards";
 
 export default function App() {
   return (
     <div className="App">
+      <Header />
       <Router>
         <Switch>
           <Route path="/">
-            <h1>Homepage Test</h1>
+            <TinderCards />
           </Route>
           <Route path="/chat">
             <h1>Chat Test</h1>
           </Route>
         </Switch>
-        <Header />
       </Router>
     </div>
   );
---
+++ src/Components/TinderCards/TinderCards.js
@@ -0,0 +1,45 @@
+import React from "react";
+import TinderCard from 'react-tinder-card';
+import './TinderCards.css'
+
+
+function TinderCards() {
+  const [people, setPeople] = useState{[
+    { 
+      name: 'alberta', 
+      url: 'https://i.kym-cdn.com/photos/images/original/001/316/888/f81.jpeg'
+    },{ 
+      name: 'cotswald', 
+      url: 'https://g.foolcdn.com/editorial/images/553815/line-on-chart-falling-stock-down.jpg'
+    }
+  ]};
+  
+  return (
+    <div>
+      <h1>test Cards</h1>
+      {people.map(person => (
+        <TinderCard
+          className="swipe"
+          key={person.name}
+          preventSwipe={['up', 'down']}
+        >
+          <div 
+            style={backgroundImage: `url(${person.url})`}
+            className="card"
+          >
+            <h3>{person.name}</h3>
+          </div>
+        </TinderCard>
+      ))}
+    </div>
+  )
+}
+
+export default TinderCards;
+
+
+//not great way of pushing to array in react
+  //const people = [];
+  //people.push('alberta', 'cotswald')
+//push to array in React
+  //setPeople([...people, 'alberta', 'cotswald])
