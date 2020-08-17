796af8a0f87d50028cc508596ed2e0e3cba6f8e8
Conner McCabe <52429712+ConnerMcCabe@users.noreply.github.com>
2020-08-17 16:07:02 -0500

    adding card styling

--- src/Components/TinderCards/TinderCards.js
+++ src/Components/TinderCards/TinderCards.js
@@ -1,30 +1,31 @@
-import React from "react";
-import TinderCard from 'react-tinder-card';
-import './TinderCards.css'
-
+import React, { useState } from "react";
+import TinderCard from "react-tinder-card";
+import "./TinderCards.css";
 
 function TinderCards() {
-  const [people, setPeople] = useState{[
-    { 
-      name: 'alberta', 
-      url: 'https://i.kym-cdn.com/photos/images/original/001/316/888/f81.jpeg'
-    },{ 
-      name: 'cotswald', 
-      url: 'https://g.foolcdn.com/editorial/images/553815/line-on-chart-falling-stock-down.jpg'
+  const [people, setPeople] = useState([
+    {
+      name: "alberta",
+      url: "https://i.kym-cdn.com/photos/images/original/001/316/888/f81.jpeg"
+    },
+    {
+      name: "cotswald",
+      url:
+        "https://g.foolcdn.com/editorial/images/553815/line-on-chart-falling-stock-down.jpg"
     }
-  ]};
-  
+  ]);
+
   return (
     <div>
       <h1>test Cards</h1>
-      {people.map(person => (
+      {people.map((person) => (
         <TinderCard
           className="swipe"
           key={person.name}
-          preventSwipe={['up', 'down']}
+          preventSwipe={["up", "down"]}
         >
-          <div 
-            style={backgroundImage: `url(${person.url})`}
+          <div
+            style={{ backgroundImage: `url(${person.url})` }}
             className="card"
           >
             <h3>{person.name}</h3>
@@ -32,14 +33,13 @@ function TinderCards() {
         </TinderCard>
       ))}
     </div>
-  )
+  );
 }
 
 export default TinderCards;
 
-
 //not great way of pushing to array in react
-  //const people = [];
-  //people.push('alberta', 'cotswald')
+//const people = [];
+//people.push('alberta', 'cotswald')
 //push to array in React
-  //setPeople([...people, 'alberta', 'cotswald])
+//setPeople([...people, 'alberta', 'cotswald])
