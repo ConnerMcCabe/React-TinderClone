3cc2288796689ff2edb4e3746b903e5e4e00ea68
Conner McCabe <52429712+ConnerMcCabe@users.noreply.github.com>
2020-08-17 10:52:17 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      
+    </div>
+  );
+}
+
+
+/* 
+Header {account info, logo, message}
+<-left swipe/ current tile/ right swipe-> 
+touch bar {undo btn, ls btn, mark btn, rs btn}
+*/
